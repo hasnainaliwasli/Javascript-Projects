4ac5ca5e4b3605ee44f870b08d73d79b3ea6c1c0
hasnainaliwasli <hasnain62712@gmail.com>
2024-05-16 23:58:04 +0500

    Netflix Clone

--- Clones/Netflix/script.js
+++ Clones/Netflix/script.js
@@ -2,15 +2,35 @@
 const apikey = '17c6301991dc718f21d3d80979326a44'
 const apiEndPoint = "https://api.themoviedb.org/3"
 const apiPaths = {
-    fetchAllCategories: `${apiEndPoint}/genre/movie/list?api_key=${apikey}`
+    fetchAllCategories: `${apiEndPoint}/genre/movie/list?api_key=${apikey}`,
+    fetchMoviesList:  `${apiEndPoint}/genre/movie/list?api_key=${apikey}`
+
 }
 
 
 function init() {
+    fetchAndBuildAllSections()
+}
+
+function fetchAndBuildAllSections() {
     fetch(apiPaths.fetchAllCategories)
         .then(res => res.json())
-        .then(res => console.table(res.genres))
+        .then(res => {
+            const categories = res.genres;
+            if (Array.isArray(categories) && categories.length) {
+                categories.forEach(category => {
+                    fethAndBuildMovieSection(category)
+                });
+
+            }
+            // console.table(category);
+        })
         .catch(err => console.log(err))
+}
+
+
+function fethAndBuildMovieSection(category) {
+    console.log(category);
 }
 
 
