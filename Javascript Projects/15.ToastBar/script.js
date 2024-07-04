437693050e7b425169b3aae2ffb542d40413a6d9
hasnainaliwasli <hasnain62712@gmail.com>
2024-07-04 13:53:39 +0500

    Toast_App

--- Javascript Projects/15.ToastBar/script.js
+++ Javascript Projects/15.ToastBar/script.js
@@ -1,1 +1,13 @@
 
+
+
+
+let toastBox = document.getElementById('toastBox')
+
+function showMsg(){
+    let toast = document.createElement('div');
+    toast.classList.add('toast','text-dark', 'show');
+    toast.innerText ='Hello world'
+    toastBox.appendChild(toast)
+
+}
