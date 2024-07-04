ad0a049bd8b7face5856f30b0b895d8217d6248c
hasnainaliwasli <hasnain62712@gmail.com>
2024-07-04 18:53:36 +0500

    Toast_App

--- Javascript Projects/15.ToastBar/script.js
+++ Javascript Projects/15.ToastBar/script.js
@@ -3,11 +3,24 @@
 
 
 let toastBox = document.getElementById('toastBox')
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i> Error encountered! Fix it'
+let successMsg = '<i class="fa-solid fa-circle-check"></i> Successfully SUbmited!'
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid Input,Check again!'
 
-function showMsg(){
+function showMsg(msg) {
     let toast = document.createElement('div');
-    toast.classList.add('toast','text-dark', 'show');
-    toast.innerText ='Hello world'
+    toast.classList.add('toast', 'text-dark', 'show');
+    if(msg.includes('Error')){
+        toast.classList.add('error')
+    }
+    if(msg.includes('Invalid')){
+        toast.classList.add('invalid')
+    }
+    toast.innerHTML = msg
     toastBox.appendChild(toast)
 
+    console.log(toastBox);
 }
+
+
+
